36171b3caab97528589799cc7fb7c1ca3a338a89
Shane Wade <shane.au.wade@gmail.com>
2025-11-17 22:09:04 -0800

    trying out another apporach to pipe env vars from deno deploy into astro build

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "astro/config"
+import { defineConfig, envField } from "astro/config"
 import react from "@astrojs/react"
 import deno from "@deno/astro-adapter"
 import UnoCSS from "unocss/astro"
+import process from "node:process"
 
 // https://astro.build/config
 export default defineConfig({
@@ -30,6 +31,21 @@ export default defineConfig({
           "./src",
         ],
       },
+    },
+  },
+  env: {
+    schema: {
+      PUBLIC_SUPABASE_URL: envField.string({
+        context: "client",
+        access: "public",
+        default: process.env.PUBLIC_SUPABASE_URL ?? "http://127.0.0.1:54321",
+      }),
+      PUBLIC_SUPABASE_ANON_KEY: envField.string({
+        context: "client",
+        access: "public",
+        default: process.env.PUBLIC_SUPABASE_ANON_KEY ??
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+      }),
     },
   },
 })
